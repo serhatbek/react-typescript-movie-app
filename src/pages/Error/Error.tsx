49d58cfffc2b9dbc3ed415438d400cf74b4889cd
serhatbek <serhat.bekk@gmail.com>
2024-01-31 14:44:29 +0300

    add error page

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,9 +1,20 @@
+import './Error.scss';
 import { useRouteError } from 'react-router-dom';
 
 const Error = () => {
-  const error = useRouteError();
-  console.log('error', error);
-  return <div></div>;
+  const error: any = useRouteError();
+  // console.log('error', error);
+  return (
+    <section className='section section-error'>
+      <div className='container'>
+        <h2>Oops!</h2>
+        <h3>{error.status}</h3>
+        <p>{error.statusText}</p>
+        <p>{error.message}</p>
+        <p>Sorry, an unexpected error has occurred.</p>
+      </div>
+    </section>
+  );
 };
 
 export default Error;
